753274e529c6b40b70d1cc5af8b9aa0d677453ff
kurokimai <164568171+kurokimai@users.noreply.github.com>
2024-07-20 16:29:57 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $product_name = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    //連想配列を出力する
+    print_r($product_name);
+
+    ?>
+    </p>
+  
+</body>
+</html>
